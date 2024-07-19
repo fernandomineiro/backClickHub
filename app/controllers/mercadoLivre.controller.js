9bd87824af675fa8c0c006b60b254df5355ee940
Fernando Fernandes <fernando@parceirogoogle.com.br>
2024-07-19 20:50:32 -0300

    up

--- app/controllers/mercadoLivre.controller.js
+++ app/controllers/mercadoLivre.controller.js
@@ -3,7 +3,7 @@ const CLIENT_SECRET = 'qy9QJn4pUWuzvBtDu2VFOjrW9hIp69Os';
 // let CLIENT_ID = '';
 // let CLIENT_SECRET = '';
 // let email = '';
-let REDIRECT_URI = 'https://backclickhub-e83be6f85c9d.herokuapp.com/api/callback';
+const REDIRECT_URI = 'https://backclickhub-e83be6f85c9d.herokuapp.com/api/callback';
 const axios = require('axios');
 const opn = require('opn');
 const nodemailer = require('nodemailer');
@@ -12,7 +12,6 @@ const nodemailer = require('nodemailer');
 let clientData = {};
 
 exports.start = async (req, res) => {
-
 
   const url = `https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
   
@@ -30,65 +29,78 @@ exports.start = async (req, res) => {
       return res.status(400).send('Código de autorização não fornecido.');
     }
 
+    console.log(code, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
     const email = 'fernandofitilan@hotmail.com'
 
-    axios.post('https://api.mercadolibre.com/oauth/token', null, {
-      params: {
-        grant_type: 'authorization_code',
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        code: code,
-        redirect_uri: REDIRECT_URI
-      }
-    })
-    .then(response => {
-      const accessToken = response.data.access_token;
-
-
-      const transporter = nodemailer.createTransport({
-        service: 'hotmail',
-        auth: {
-          user: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
-          pass: 'clickhub123@' // Sua senha do Hotmail
-        }
+    try {
+      const tokenResponse = await axios.post('https://api.mercadolibre.com/oauth/token', null, {
+          params: {
+              grant_type: 'authorization_code',
+              client_id: CLIENT_ID,
+              client_secret: CLIENT_SECRET,
+              code: code,
+              redirect_uri: REDIRECT_URI
+          }
       });
+
+      const accessToken = tokenResponse.data.access_token;
+      res.json({ access_token: accessToken });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+
+     
+
+    // axios.post('https://api.mercadolibre.com/oauth/token', null, {
+    //   params: {
+    //     grant_type: 'authorization_code',
+    //     client_id: CLIENT_ID,
+    //     client_secret: CLIENT_SECRET,
+    //     code: code,
+    //     redirect_uri: REDIRECT_URI
+    //   }
+    // })
+    // .then(async response => {
+    //   const accessToken = response.data.access_token;
+
+    //   console.log(accessToken)
+
+    // res.json({ access_token: accessToken });
+
+      // const transporter = nodemailer.createTransport({
+      //   service: 'hotmail',
+      //   auth: {
+      //     user: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
+      //     pass: 'clickhub123@' // Sua senha do Hotmail
+      //   }
+      // });
       
-      // Configurar os detalhes do email
-      const mailOptions = {
-        from: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
-        to: `${email}`, // Email do destinatário
-        subject: 'Seu token de acesso!',
-        text: `${dados}`,
-        html: `<h1>${accessToken}</h1>` // Corpo do email em HTML
-      };
+
+      // const mailOptions = {
+      //   from: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
+      //   to: `${email}`, // Email do destinatário
+      //   subject: 'Seu token de acesso!',
+      //   text: `${dados}`,
+      //   html: `<h1>${accessToken}</h1>` // Corpo do email em HTML
+      // };
       
-      // Enviar o email
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          const novaUrl = 'http://localhost:3000/#/integracao';
-          res.redirect(novaUrl);
-        } else {
-          console.log('Email enviado:', info.response);
+
+      // transporter.sendMail(mailOptions, function(error, info){
+      //   if (error) {
+      //     const novaUrl = 'http://localhost:3000/#/integracao';
+      //     res.redirect(novaUrl);
+      //   } else {
+      //     console.log('Email enviado:', info.response);
     
-        }
-      });
-
-
-
-
-
-
-      res.json({
-        message: 'Token de acesso obtido com sucesso',
-        accessToken: accessToken
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: 'Erro ao obter o token de acesso',
-        error: error.response.data
-      });
-    });
+      //   }
+      // });
+    // })
+    // .catch(error => {
+    //   console.log(error.response)
+    //   res.status(500).json({
+    //     message: 'Erro ao obter o token de acesso',
+    //   });
+    // });
   };
   
 

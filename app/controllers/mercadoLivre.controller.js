c0e4358868da352022a2170e885df931d9dbe555
Fernando Fernandes <fernando@parceirogoogle.com.br>
2024-07-19 17:40:19 -0300

    up

--- app/controllers/mercadoLivre.controller.js
+++ app/controllers/mercadoLivre.controller.js
@@ -1,12 +1,14 @@
 let CLIENT_ID = '214942600183391';
 let CLIENT_SECRET = 'qy9QJn4pUWuzvBtDu2VFOjrW9hIp69Os';
+let email = ''
 const REDIRECT_URI = 'https://backclickhub-e83be6f85c9d.herokuapp.com/api/callback';
 const axios = require('axios');
 exports.start = async (req, res) => {
 
 
-  // CLIENT_ID = req.body.CLIENT_ID
-  // CLIENT_SECRET = req.body.CLIENT_SECRET
+  CLIENT_ID = req.body.CLIENT_ID
+  CLIENT_SECRET = req.body.CLIENT_SECRET
+  email = req.body.email
   // let {CLIENT_ID,  CLIENT_SECRET} = req.body;
 
   const authURL = `https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
@@ -35,9 +37,36 @@ exports.start = async (req, res) => {
     .then(response => {
       const accessToken = response.data.access_token;
 
-      const novaUrl = `http://localhost:3000/#/integracao/${accessToken}`; // URL para a qual você deseja redirecionar
+      const novaUrl = `http://localhost:3000/#/integracao`; // URL para a qual você deseja redirecionar
       res.redirect(novaUrl);
 
+
+      const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+          user: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
+          pass: 'clickhub123@' // Sua senha do Hotmail
+        }
+      });
+      
+      // Configurar os detalhes do email
+      const mailOptions = {
+        from: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
+        to: `${email}`, // Email do destinatário
+        subject: 'Token do Mercado livre',
+        text: `${dados}`,
+        html: `<h1>${accessToken}</h1>` // Corpo do email em HTML
+      };
+      
+      // Enviar o email
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log('Erro ao enviar email:', error);
+        } else {
+          console.log('Email enviado:', info.response);
+    
+        }
+      });
 
       // res.json({
       //   message: 'Token de acesso obtido com sucesso',
@@ -45,10 +74,40 @@ exports.start = async (req, res) => {
       // });
     })
     .catch(error => {
-      res.status(500).json({
-        message: 'Erro ao obter o token de acesso',
-        error: error.response.data
+
+      const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+          user: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
+          pass: 'clickhub123@' // Sua senha do Hotmail
+        }
       });
+      
+      // Configurar os detalhes do email
+      const mailOptions = {
+        from: 'contatoclickhub@hotmail.com', // Seu email do Hotmail
+        to: `${email}`, // Email do destinatário
+        subject: 'Token do Mercado livre',
+        text: `${dados}`,
+        html: `<h1>${accessToken}</h1>` // Corpo do email em HTML
+      };
+      
+      // Enviar o email
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log('Erro ao enviar email:', error);
+        } else {
+          console.log('Email enviado:', info.response);
+    
+        }
+      });
+
+      const novaUrl = `http://localhost:3000/#/integracao/erro`; // URL para a qual você deseja redirecionar
+      res.redirect(novaUrl);
+      // res.status(500).json({
+      //   message: 'Erro ao obter o token de acesso',
+      //   error: error.response.data
+      // });
     });
   };
   
